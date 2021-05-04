2872013a8cee445db93cc43fad9553b8362ac3bf
Carlos Domingues <crdpc@yahoo.com.br>
2021-05-04 00:10:36 -0300

    Bootstrap

--- frontend/.history/src/index_20210504000800.tsx
+++ frontend/.history/src/index_20210504000800.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'assets/css/styles.css';
+import 'assets/css/styles';
 import App from './App';
 
 
