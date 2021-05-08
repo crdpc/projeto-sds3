896df4c17b2b975413ca0e0ce09835010acf9b95
Carlos Domingues <crdpc@yahoo.com.br>
2021-05-08 18:56:32 -0300

    First request

---
+++ frontend/.history/src/components/DonutChart/index_20210508182937.tsx
@@ -0,0 +1,37 @@
+import Chart from 'react-apexcharts';
+
+type ChartData (){
+    labels: string[];
+    series: number[];
+    
+}
+
+function DonutChart() => {
+
+    let chartData : ChartData = {labels: [], series: []};
+
+    const mockData = {
+        series: [477138, 499928, 444867, 220426, 473088],
+        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
+    }
+    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+
+    return (
+
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
+
+        />
+
+    );
+}
+
+export default DonutChart;
